7721030a2ee22dfff5b805ec5725d6b6a74ae328
urimanul <myopera9@gmail.com>
2024-11-03 16:41:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ with tabs[1]:
     }
 
     # ボタンが押されたときの処理
-    if st.button("生成"):
+    if st.button("RAG生成"):
         # POSTデータを設定
         my_content = f"{rag_chain} 必ず、日本語で答えてください。"
         data = {
